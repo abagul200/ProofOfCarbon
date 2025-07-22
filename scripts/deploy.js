05bcd33ab1dac73fe35561b4ede743f406d94949
abagul200 <77838527+abagul200@users.noreply.github.com>
2025-07-22 15:17:06 +0530

    Update deploy.js

--- scripts/deploy.js
+++ scripts/deploy.js
@@ -1,11 +1,14 @@
 const hre = require("hardhat");
 
+
 async function main() {
   const ProofOfCarbon = await hre.ethers.getContractFactory("ProofOfCarbon");
   const proofOfCarbon = await ProofOfCarbon.deploy();
 
+  
   await proofOfCarbon.deployed();
 
+  
   console.log(
     `ProofOfCarbon deployed to Core Blockchain at address: ${proofOfCarbon.address}`
   );

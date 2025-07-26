2b1c63f637c368a7d6020761a5d8cef7ae934039
abagul200 <77838527+abagul200@users.noreply.github.com>
2025-07-26 22:14:30 +0530

    Update hardhat.config.js

--- hardhat.config.js
+++ hardhat.config.js
@@ -2,9 +2,11 @@
  * @type import('hardhat/config').HardhatUserConfig
  */
 
+
 require("@nomiclabs/hardhat-waffle");
 require("@nomiclabs/hardhat-ethers");
 require("dotenv").config();
+
 
 module.exports = {
   solidity: "0.8.17",
